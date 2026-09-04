d7229ccd3e5e0efa570bf9edaa052dd46ed641db
Basmala Elbagory <elbagoryb@gmail.com>
2026-09-04 16:47:25 +0300

    Fix recipe images and update project

--- tasty-recipes/src/data.js
+++ tasty-recipes/src/data.js
@@ -1,87 +1,98 @@
-
-
 const recipes = [
   {
     id: 1,
     name: "Creamy Tomato Pasta",
     category: "MAIN DISH",
     time: "30 min",
-    image: "pasta.jpg",
-    description: "A creamy and comforting pasta made with fresh tomatoes and herbs. ",
+    image: "/Tasty-Recipes/images/pasta.jpg",
+    description:
+      "A creamy and comforting pasta made with fresh tomatoes and herbs.",
     isPopular: true,
-    Chef:true
+    Chef: true
   },
+
   {
     id: 2,
     name: "Grilled Chicken",
     category: "MAIN DISH",
     time: "40 min",
-    image: "chicken.jpg",
-    description: "Juicy grilled chicken with a blend of herbs and spices. ",
+    image: "/Tasty-Recipes/images/chicken.jpg",
+    description:
+      "Juicy grilled chicken with a blend of herbs and spices.",
     isPopular: false,
-    Chef:false
-  }
-  ,
-  { 
+    Chef: false
+  },
+
+  {
     id: 3,
     name: "Chocolate Cake",
     category: "DESSERT",
     time: "45 min",
-    image: "cake.jpg",
+    image: "/Tasty-Recipes/images/cake.jpg",
     description: "A soft chocolate cake.",
     isPopular: true,
-    Chef:true
-  }
-  ,{
+    Chef: true
+  },
+
+  {
     id: 4,
     name: "Fluffy Pancakes",
     category: "DESSERT",
     time: "20 min",
-    image: "pancake.jpg",
-    description: "Soft and fluffy pancakes perfect for a sweet morning.",
+    image: "/Tasty-Recipes/images/pancake.jpg",
+    description:
+      "Soft and fluffy pancakes perfect for a sweet morning.",
     isPopular: true,
-    Chef:false
-  }
-  ,
+    Chef: false
+  },
+
   {
     id: 5,
     name: "Greek Salad",
     category: "SALAD",
     time: "15 min",
-    image: "salad.jpg",
-    description: "Fresh and healthy salad with classic Greek flavors.",
+    image: "/Tasty-Recipes/images/salad.jpg",
+    description:
+      "Fresh and healthy salad with classic Greek flavors.",
     isPopular: false,
-    Chef:false
-  }
-  ,
-  {id: 6,
+    Chef: false
+  },
+
+  {
+    id: 6,
     name: "Garlic Bread",
     category: "APPETIZER",
     time: "15 min",
-    image: "bread.jpg",
-    description: "Crispy on the outside , soft on the inside with a rich garlic flavor.",
+    image: "/Tasty-Recipes/images/bread.jpg",
+    description:
+      "Crispy on the outside, soft on the inside with a rich garlic flavor.",
     isPopular: false,
-    Chef:false
-  }
-  ,
-  {id: 7,
+    Chef: false
+  },
+
+  {
+    id: 7,
     name: "Strawberry Cheesecake",
     category: "DESSERT",
     time: "45 min",
-    image: "src/images/Cheesecake.jpg",
-    description: "A creamy and delicious cheesecake topped with fresh strawberries and a sweet strawberry sauce. Perfectly smooth, refreshing, and irresistible. ",
+    image: "/Tasty-Recipes/images/Cheesecake.jpg",
+    description:
+      "A creamy and delicious cheesecake topped with fresh strawberries and a sweet strawberry sauce. Perfectly smooth, refreshing, and irresistible.",
     isPopular: false,
-    Chef:false
-  }
-  ,
-   {id: 8,
+    Chef: false
+  },
+
+  {
+    id: 8,
     name: "Cannelloni",
     category: "MAIN DISH",
     time: "60 min",
-    image: "src/images/cannoli.jpg",
-    description: "Stuffed pasta tubes filled with creamy ricotta and spinach, topped with rich tomato saice and melted cheese. ",
+    image: "/Tasty-Recipes/images/cannoli.jpg",
+    description:
+      "Stuffed pasta tubes filled with creamy ricotta and spinach, topped with rich tomato sauce and melted cheese.",
     isPopular: false,
-    Chef:false
+    Chef: false
   }
 ];
-export{recipes};
+
+export { recipes };
